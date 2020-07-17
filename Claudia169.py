882e95ca698572c8d89c543695d92b04ecb0fa6f
Claudia Moron Fernandez <claudya169@gmail.com>
2020-07-17 10:20:39 -0500

    Haciendo atributos

--- Claudia169.py
+++ Claudia169.py
@@ -3,6 +3,17 @@ class Mouse:
         self._marca = marca
         self._color = color
         self._tipo = tipo
-
     @property
     def marca (self):
+        return self._marca
+
+    @property
+    def color (self):
+        return self._color
+
+    @property
+    def color (self):
+        return self._tipo
+
+    def imprimir (self):
+        print ("Mouse existente")
