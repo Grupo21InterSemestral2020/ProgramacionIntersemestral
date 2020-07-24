8bb5d7ed60edff32869eb18ae6a0e43c92285ffe
Johan Alexis Balleza <johanballeza35@gmail.com>
2020-07-23 22:33:49 -0500

    fix

--- PIA/empleado.py
+++ PIA/empleado.py
@@ -35,7 +35,6 @@ class Empleado:
         with open("./BD/empleados.txt","r",encoding="utf8") as empleados:
             num = empleados.readlines()
             for f in num:
-<<<<<<< HEAD
                 while self.__idEmpleado in f: 
                     self.__idEmpleado = input("Lo siento, ese ID ya existe, ingrese otro:\n")
                 empleados.close()
@@ -68,78 +67,3 @@ E = Empleado(0,0,0)
 E.AgregarEmpleado()
 E.AgregarEmpleado()
 E.EliminarEmpleado()
-=======
-                while idEmpleado in f: 
-                    idEmpleado = input("Lo siento, ese ID ya existe, ingrese otro:\n")
-            Nombre = str(input("Ingresa su Nombre:\n"))
-            Direccion = str(input("Ingresa su Dirección:\n"))
-<<<<<<< HEAD
-            archivo.write(f"{idEmpleado} | {Nombre} | {Direccion}\n")
-            archivo.close
-    
-    def EliminarEmpleado():
-        empleados = open("./BD/empleados.txt","r",encoding="utf8")
-        num = empleados.readlines()
-        empleados = open("./BD/empleados.txt","w",encoding="utf8")
-        idEmpleado = input("Ingrese el ID a Eliminar: ")
-        for f in num:
-            id1 = f.split("|")[0]
-            id2 = f.split("|")[1]
-            if idEmpleado!= id1:
-               empleados.write(id2)
-        empleados.close()
-
-    def ModificarEmpleado():
-        pass
-
-Empleado.AgregarEmpleado()
-with open("./BD/empleados.txt","r",encoding="utf8") as empleados:
-    print(empleados.readlines())
-Empleado.AgregarEmpleado()
-with open("./BD/empleados.txt","r",encoding="utf8") as empleados:
-    print(empleados.readlines())
-#Empleado.EliminarEmpleado()
-#with open("./BD/empleados.txt","r",encoding="utf8") as empleados:
-#    print(empleados.readlines())
-
-archivo = open("./BD/empleados.txt","r",encoding="utf8")
-lines = archivo.readlines()
-archivo.close()
-archivo = open("./BD/empleados.txt","w",encoding="utf8")
-idEmpleado = input("Ingrese Id de empleado a modificar: ")
-    
-for line in lines:
-    id = line.split("|")[0]
-    if idEmpleado != id:
-        archivo.write(line)
-    else:
-        print("Ingrese nuevos datos de empleado: ")
-        idEmpleado=input("Ingrese id: ")
-        Nombre=str(input("Nombre: "))
-        Direccion=str(input("Direccion:"))
-        archivo.writelines(idEmpleado + "|" + Nombre + "|" + Direccion + "\n")        
-        #<3<3<3<<33<3<3<3<3<3<3<3<3       Yaki  y  Johan    <3<3<3<3<3<3<33<3<3<3<3<33
-=======
-            empleados.write(idEmpleado + '|' + Nombre + '|' + Direccion + '\n')
-            empleados.close
-   
-    def EliminarEmpleado():
-        empleados = open("./BD/empleados.txt","r",encoding="utf8")
-        lines = empleados.readlines()
-        print(lines)
-        empleados.close()
-        empleados = open("./BD/empleados.txt","w+",encoding="utf8")
-        idEmpleado = int(input("Ingrese Id de empleado a eliminar: "))
-        for line in lines:
-            id = line.split("|")[0]
-            if line!=id:
-                empleados.write(line)
-        lines = empleados.readlines()
-        print(lines)
-        empleados.close()
-
-Empleado.AgregarEmpleado()
-Empleado.AgregarEmpleado()
-Empleado.EliminarEmpleado()
->>>>>>> ad213252f7e676a226b99266ee89b995d489de06
->>>>>>> 58f9c2b504f8fc80827c18c1a224660c039168d4
