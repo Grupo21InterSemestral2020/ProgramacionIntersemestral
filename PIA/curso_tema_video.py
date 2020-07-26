11c236e428b8a4eaf6f4f168ba0683dc1d42a703
Blanca Nallely Macias Vazquez <naychida@gmail.com>
2020-07-26 13:06:28 -0500

    Agregue video asignado

--- PIA/curso_tema_video.py
+++ PIA/curso_tema_video.py
@@ -7,23 +7,31 @@ class video_tema:
     def idCursoTv (self):
         return self.__idCursoTv
     @idCursoTv.setter
-    def idCursoTvs (self, valoridCursoTv):
-        self.__idCursoTv = valoridCursoTv
+    def idCursoTvs (self, valor):
+        self.__idCursoTv = valor
 
     @property
     def idVideo (self):
         return self.__idVideo
     @idVideo.setter
-    def idVideo (self, valoridVideo):
-        self.__idVideo = valoridVideo
+    def idVideo (self, valor):
+        self.__idVideo = valor
     
     @property
     def idCursoTema (self):
         return self.__idCursoTema
     @idCursoTema.setter
-    def idCursoTema (self, valoridCursoTema):
-        self.__idCursoTema = valoridCursoTema
+    def idCursoTema (self, valor):
+        self.__idCursoTema = valor
 
-    
+      def AgregarVideoAsignado(self):
+        self.archivo = open("./BD/curso_tema_videos.txt","a",encoding="utf8")
+        self.__idCursoTv = input("Ingrese el ID:\n")
+        with open("./BD/curso_tema_videos.txt","r",encoding="utf8") as tema:
+            num = tema.readlines()
+            for f in num:
+                while self.__idCursoTv in f: 
+                    self.__idCursoTv = input("Este ID ya existe, ingrese otro:\n")
+                curso.close()
 
 
